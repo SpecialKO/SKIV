826173696f542fa209f4d1f60a32ae07110587d4
aemony <aemony@aemony.se>
2024-06-13 21:51:05 +0200

    Adjust ImGuiNotify to our needs

--- packages_misc/ImGuiNotify.hpp
+++ packages_misc/ImGuiNotify.hpp
@@ -36,16 +36,16 @@
  * CONFIGURATION SECTION Start
 */
 
-#define NOTIFY_MAX_MSG_LENGTH				4096		// Max message content length
-#define NOTIFY_PADDING_X					20.f		// Bottom-left X padding
-#define NOTIFY_PADDING_Y					00.f		// Bottom-left Y padding
-#define NOTIFY_PADDING_MESSAGE_Y			10.f		// Padding Y between each message
-#define NOTIFY_FADE_IN_OUT_TIME				150			// Fade in and out duration
-#define NOTIFY_DEFAULT_DISMISS				3000		// Auto dismiss after X ms (default, applied only of no data provided in constructors)
-#define NOTIFY_OPACITY						0.8f		// 0-1 Toast opacity
-#define NOTIFY_USE_SEPARATOR 				false 		// If true, a separator will be rendered between the title and the content
-#define NOTIFY_USE_DISMISS_BUTTON			true		// If true, a dismiss button will be rendered in the top right corner of the toast
-#define NOTIFY_RENDER_LIMIT					5			// Max number of toasts rendered at the same time. Set to 0 for unlimited
+#define NOTIFY_MAX_MSG_LENGTH       4096    // Max message content length
+#define NOTIFY_PADDING_X            20.f    // Bottom-left X padding
+#define NOTIFY_PADDING_Y            20.f    // Bottom-left Y padding
+#define NOTIFY_PADDING_MESSAGE_Y    10.f    // Padding Y between each message
+#define NOTIFY_FADE_IN_OUT_TIME     150     // Fade in and out duration
+#define NOTIFY_DEFAULT_DISMISS      3000    // Auto dismiss after X ms (default, applied only of no data provided in constructors)
+#define NOTIFY_OPACITY              1.f    // 0-1 Toast opacity
+#define NOTIFY_USE_SEPARATOR        false   // If true, a separator will be rendered between the title and the content
+#define NOTIFY_USE_DISMISS_BUTTON   false   // If true, a dismiss button will be rendered in the top right corner of the toast
+#define NOTIFY_RENDER_LIMIT         5       // Max number of toasts rendered at the same time. Set to 0 for unlimited
 
 /**
  * CONFIGURATION SECTION End
@@ -57,8 +57,8 @@
 
 
 
-
-static const ImGuiWindowFlags NOTIFY_DEFAULT_TOAST_FLAGS = ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoFocusOnAppearing;
+static const ImGuiChildFlags  NOTIFY_DEFAULT_CHILD_FLAGS  = ImGuiChildFlags_AlwaysAutoResize | ImGuiChildFlags_AutoResizeX | ImGuiChildFlags_AutoResizeY | ImGuiChildFlags_Border;
+static const ImGuiWindowFlags NOTIFY_DEFAULT_WINDOW_FLAGS = ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_Tooltip; // ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoNav | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoFocusOnAppearing;
 
 #define NOTIFY_NULL_OR_EMPTY(str)		(!str || !strlen(str))
 #define NOTIFY_FORMAT(fn, format, ...)	if (format) {va_list args; va_start(args, format); fn(format, args, ##__VA_ARGS__); va_end(args);}
@@ -100,7 +100,7 @@ enum class ImGuiToastPos : uint8_t
 class ImGuiToast
 {
 private:
-	ImGuiWindowFlags							flags = NOTIFY_DEFAULT_TOAST_FLAGS;
+	ImGuiWindowFlags							flags = NOTIFY_DEFAULT_WINDOW_FLAGS;
 
 	ImGuiToastType								type = ImGuiToastType::None;
 	char										title[NOTIFY_MAX_MSG_LENGTH];
@@ -412,6 +412,8 @@ public:
 
 		memset(this->title, 0, sizeof(this->title));
 		memset(this->content, 0, sizeof(this->content));
+
+    this->setTitle(this->getDefaultTitle());
 	}
 
 	/**
@@ -440,7 +442,22 @@ public:
 	}
 
 	/**
-	 * @brief Constructor for creating a new ImGuiToast object with a specified type, dismiss time, title format, content format and a button.
+	 * @brief Constructor for creating a new ImGuiToast object with a specified type, dismiss time, title, and content format.
+	 * 
+	 * @param type The type of the toast message.
+	 * @param dismissTime The time in milliseconds before the toast message is dismissed.
+	 * @param format The format string for the content of the toast message.
+	 * @param ... The variable arguments to be formatted according to the format string.
+	 */
+	ImGuiToast(ImGuiToastType type, int dismissTime, const char* titleLabel, const char* format, ...) : ImGuiToast(type, dismissTime)
+	{
+		NOTIFY_FORMAT(this->setContent, format);
+
+		this->setTitle(titleLabel);
+	}
+
+	/**
+	 * @brief Constructor for creating a new ImGuiToast object with a specified type, dismiss time, content format, content format and a button.
 	 * 
 	 * @param type The type of the toast message.
 	 * @param dismissTime The time in milliseconds before the toast message is dismissed.
@@ -490,6 +507,8 @@ namespace ImGui
 	inline void RenderNotifications()
 	{
 		const ImVec2 mainWindowSize = GetCurrentWindowRead()->Size;
+		const float  textWrapWidth  = (600.0f * SKIF_ImGui_GlobalDPIScale < mainWindowSize.x - NOTIFY_PADDING_X * 2)
+                                ?  600.0f * SKIF_ImGui_GlobalDPIScale : mainWindowSize.x - NOTIFY_PADDING_X * 2;
 
 		float height = 0.f;
 
@@ -515,7 +534,7 @@ namespace ImGui
 			const char* icon = currentToast->getIcon();
 			const char* title = currentToast->getTitle();
 			const char* content = currentToast->getContent();
-			const char* defaultTitle = currentToast->getDefaultTitle();
+		//const char* defaultTitle = currentToast->getDefaultTitle();
 			const float opacity = currentToast->getFadePercent(); // Get opacity based of the current phase
 
 			// Window rendering
@@ -542,111 +561,116 @@ namespace ImGui
 			// Set notification window flags
 			if (!NOTIFY_USE_DISMISS_BUTTON && currentToast->getOnButtonPress() == nullptr)
 			{
-				currentToast->setWindowFlags(NOTIFY_DEFAULT_TOAST_FLAGS | ImGuiWindowFlags_NoInputs);
+				currentToast->setWindowFlags(NOTIFY_DEFAULT_WINDOW_FLAGS | ImGuiWindowFlags_NoInputs);
 			}
 
-			Begin(windowName, nullptr, currentToast->getWindowFlags());
+			if (BeginChild (windowName, ImVec2 (0, 0), NOTIFY_DEFAULT_CHILD_FLAGS, currentToast->getWindowFlags()))
+      {
+			  // Render over all other windows
+			  //BringWindowToDisplayFront(GetCurrentWindow());
 
-			// Render over all other windows
-			BringWindowToDisplayFront(GetCurrentWindow());
+			  // Here we render the toast content
+			  {
+				  PushTextWrapPos(textWrapWidth); // mainWindowSize.x / 3.f // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
 
-			// Here we render the toast content
-			{
-				PushTextWrapPos(mainWindowSize.x / 3.f); // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
+				  bool wasTitleRendered = false;
 
-				bool wasTitleRendered = false;
+				  // If an icon is set
+				  if (!NOTIFY_NULL_OR_EMPTY(icon))
+				  {
+					  //Text(icon); // Render icon text
+					  TextColored(textColor, "%s", icon);
+					  wasTitleRendered = true;
+				  }
 
-				// If an icon is set
-				if (!NOTIFY_NULL_OR_EMPTY(icon))
-				{
-					//Text(icon); // Render icon text
-					TextColored(textColor, "%s", icon);
-					wasTitleRendered = true;
-				}
+				  // If a title is set
+				  if (!NOTIFY_NULL_OR_EMPTY(title))
+				  {
+					  // If a title and an icon is set, we want to render on same line
+					  if (!NOTIFY_NULL_OR_EMPTY(icon))
+						  SameLine();
 
-				// If a title is set
-				if (!NOTIFY_NULL_OR_EMPTY(title))
-				{
-					// If a title and an icon is set, we want to render on same line
-					if (!NOTIFY_NULL_OR_EMPTY(icon))
-						SameLine();
+					  Text("%s", title); // Render title text
+					  wasTitleRendered = true;
+				  }
 
-					Text("%s", title); // Render title text
-					wasTitleRendered = true;
-				} else 
-				if (!NOTIFY_NULL_OR_EMPTY(defaultTitle))
-				{
-					if (!NOTIFY_NULL_OR_EMPTY(icon))
-						SameLine();
+#if 0
+          else
+				  if (!NOTIFY_NULL_OR_EMPTY(defaultTitle))
+				  {
+					  if (!NOTIFY_NULL_OR_EMPTY(icon))
+						  SameLine();
 
-					Text("%s", defaultTitle); // Render default title text (ImGuiToastType_Success -> "Success", etc...)
-					wasTitleRendered = true;
-				}
+					  Text("%s", defaultTitle); // Render default title text (ImGuiToastType_Success -> "Success", etc...)
+					  wasTitleRendered = true;
+				  }
+#endif
 
-				// If a dismiss button is enabled
-				if (NOTIFY_USE_DISMISS_BUTTON)
-				{
-					// If a title or content is set, we want to render the button on the same line
-					if (wasTitleRendered || !NOTIFY_NULL_OR_EMPTY(content))
-					{
-						SameLine();
-					}
+				  // If a dismiss button is enabled
+				  if (NOTIFY_USE_DISMISS_BUTTON)
+				  {
+					  // If a title or content is set, we want to render the button on the same line
+					  if (wasTitleRendered || !NOTIFY_NULL_OR_EMPTY(content))
+					  {
+						  SameLine();
+					  }
 
-					// Render the dismiss button on the top right corner
-					// NEEDS TO BE REWORKED
-					float scale = 0.8f;
+					  // Render the dismiss button on the top right corner
+					  // NEEDS TO BE REWORKED
+					  float scale = 0.8f;
 
-					if (CalcTextSize(content).x > GetWindowContentRegionMax().x)
-					{
-						scale = 0.95f;
-					}
+					  if (CalcTextSize(content).x > GetWindowContentRegionMax().x)
+					  {
+						  scale = 0.95f;
+					  }
 
-					SetCursorPosX(GetCursorPosX() + (GetWindowSize().x - GetCursorPosX()) * scale);
+					  SetCursorPosX(GetCursorPosX() + (GetWindowSize().x - GetCursorPosX()) * scale);
 
-					// If the button is pressed, we want to remove the notification
-					if (Button(ICON_FA_XMARK))
-					{
-						RemoveNotification(i);
-					}
-				}
+					  // If the button is pressed, we want to remove the notification
+					  if (Button(ICON_FA_XMARK))
+					  {
+						  RemoveNotification(i);
+					  }
+				  }
 
-				// In case ANYTHING was rendered in the top, we want to add a small padding so the text (or icon) looks centered vertically
-				if (wasTitleRendered && !NOTIFY_NULL_OR_EMPTY(content))
-				{
-					SetCursorPosY(GetCursorPosY() + 5.f); // Must be a better way to do this!!!!
-				}
+				  // In case ANYTHING was rendered in the top, we want to add a small padding so the text (or icon) looks centered vertically
+				  if (wasTitleRendered && !NOTIFY_NULL_OR_EMPTY(content))
+				  {
+					  SetCursorPosY(GetCursorPosY() + 5.f); // Must be a better way to do this!!!!
+				  }
 
-				// If a content is set
-				if (!NOTIFY_NULL_OR_EMPTY(content))
-				{
-					if (wasTitleRendered)
-					{
-						#if NOTIFY_USE_SEPARATOR
-							Separator();
-						#endif
-					}
+				  // If a content is set
+				  if (!NOTIFY_NULL_OR_EMPTY(content))
+				  {
+					  if (wasTitleRendered)
+					  {
+						  #if NOTIFY_USE_SEPARATOR
+							  Separator();
+						  #endif
+					  }
 
-					Text("%s", content); // Render content text
-				}
+					  Text("%s", content); // Render content text
+				  }
 
-				// If a button is set
-				if (currentToast->getOnButtonPress() != nullptr)
-				{
-					// If the button is pressed, we want to execute the lambda function
-					if (Button(currentToast->getButtonLabel()))
-					{
-						currentToast->getOnButtonPress()();
-					}
-				}
+				  // If a button is set
+				  if (currentToast->getOnButtonPress() != nullptr)
+				  {
+					  // If the button is pressed, we want to execute the lambda function
+					  if (Button(currentToast->getButtonLabel()))
+					  {
+						  currentToast->getOnButtonPress()();
+					  }
+				  }
 
-				PopTextWrapPos();
-			}
+				  PopTextWrapPos();
+			  }
 
-			// Save height for next toasts
-      height += GetWindowHeight() + NOTIFY_PADDING_MESSAGE_Y;
+			  // Save height for next toasts
+        height += GetWindowHeight() + NOTIFY_PADDING_MESSAGE_Y;
 
-			// End
-			End();
+			  // End
+			  EndChild();
+      }
 		}
 	}
 }
